e295fa32aee4df198147ce364427e5792c045e70
Josuepma <1930168@upv.edu.mx>
2022-06-06 09:14:00 -0500

    Using database object

--- app/src/main/java/com/upv/pm_2022/iti_27856_equipo_04/MainActivity.java
+++ app/src/main/java/com/upv/pm_2022/iti_27856_equipo_04/MainActivity.java
@@ -1,13 +1,25 @@
 package com.upv.pm_2022.iti_27856_equipo_04;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.TableLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String dbName = "merch.db";
+    private DataBase usdbh;
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usdbh = new DataBase(this, dbName, null, 1);
+        db = usdbh.getWritableDatabase();
     }
 }
