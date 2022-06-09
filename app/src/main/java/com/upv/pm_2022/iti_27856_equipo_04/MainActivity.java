84f584e672b8625051ed380ccfb3a105af6281ea
Josuepma <1930168@upv.edu.mx>
2022-06-09 01:05:14 -0500

    MainActivity only with tabs

--- app/src/main/java/com/upv/pm_2022/iti_27856_equipo_04/MainActivity.java
+++ app/src/main/java/com/upv/pm_2022/iti_27856_equipo_04/MainActivity.java
@@ -1,25 +1,40 @@
 package com.upv.pm_2022.iti_27856_equipo_04;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.widget.TableLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private final String dbName = "merch.db";
-    private DataBase usdbh;
-    private SQLiteDatabase db;
-    private Cursor cursor;
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        usdbh = new DataBase(this, dbName, null, 1);
-        db = usdbh.getWritableDatabase();
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.ViewPager);
+        tabLayout.addTab(tabLayout.newTab().setText("Brand"));
+        tabLayout.addTab(tabLayout.newTab().setText("Product"));
+        tabLayout.addTab(tabLayout.newTab().setText("Comparative"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        String[] tabs = new String[]{"Brand","Product","Comparative"};
+        new TabLayoutMediator(tabLayout, viewPager2,
+                (tab, position) -> tab.setText(tabs[position])
+        ).attach();
+
     }
+
+
 }
