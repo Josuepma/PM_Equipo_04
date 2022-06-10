94496578bfeb8a74770ee21fc6a2af9ae4c580d9
Josuepma <1930168@upv.edu.mx>
2022-06-10 17:57:10 -0500

    Constructor, getters, setters, toString

--- app/src/main/java/com/upv/pm_2022/iti_27856_equipo_04/Price.java
+++ app/src/main/java/com/upv/pm_2022/iti_27856_equipo_04/Price.java
@@ -1,2 +1,56 @@
-package com.upv.pm_2022.iti_27856_equipo_04;public class Price {
+package com.upv.pm_2022.iti_27856_equipo_04;
+
+public class Price {
+    private int id;
+    private double price;
+    private Store store;
+    private Product product;
+
+    public Price(double price,Store store, Product product){
+        setPrice(price);
+        setStore(store);
+        setProduct(product);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "Price{" +
+                "id=" + id +
+                ", price=" + price +
+                ", store=" + store +
+                ", product=" + product +
+                '}';
+    }
 }
