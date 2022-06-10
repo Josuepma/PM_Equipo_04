5559f12f4768425970266d95337b0d16ccea5ee4
Josuepma <1930168@upv.edu.mx>
2022-06-10 17:57:24 -0500

    Constructor, getters, setters, toString

--- app/src/main/java/com/upv/pm_2022/iti_27856_equipo_04/Store.java
+++ app/src/main/java/com/upv/pm_2022/iti_27856_equipo_04/Store.java
@@ -1,2 +1,45 @@
-package com.upv.pm_2022.iti_27856_equipo_04;public class Store {
+package com.upv.pm_2022.iti_27856_equipo_04;
+
+public class Store {
+    private int id;
+    private String name;
+    private String address;
+
+    public Store(String name,String address){
+        setName(name);
+        setAddress(address);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
